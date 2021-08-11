bab272389bc9334942f750ebb9cfac920b2070d0
Kristina Peršić <72509184+Kristina456@users.noreply.github.com>
2021-08-11 16:29:08 +0200

    OilGasProvider style

--- src/components/oil-and-gas-providers/OilGasProviders.tsx
+++ src/components/oil-and-gas-providers/OilGasProviders.tsx
@@ -7,19 +7,26 @@ export type CardOilGasProviders = {
     location: string;
 }
 
+const colors = ["rgba(0, 82, 180, 1)", "rgba(255, 81, 81, 1)", "rgba(232, 111, 27, 1)", "rgba(255, 181, 71, 1)", "rgba(69, 150, 209, 1)", "rgba(0, 82, 180, 1)"];
+const backgroundColors = ["rgba(0, 82, 180, 0.5)", "rgba(255, 81, 81, 0.5)", "rgba(232, 111, 27, 0.5)", "rgba(255, 181, 71, 0.5)", "rgba(69, 150, 209, 0.5)", "rgba(0, 82, 180, 0.5)"];
+
 export default function OilGasProviders({ data }: { data: CardOilGasProviders[] }) {
 
     return (
-        <div>
-            <div>Oil & Gas Providers </div>
+        <div className="oil-gas-providers">
+            <div className="title">Oil & Gas Providers </div>
             <table>
                 {
-                    data.map((info) => {
+                    data.map((info, i) => {
                         return (
                             <tr>
-                                <td>{info.name.charAt(0)}</td>
-                                <td>{info.name}</td>
-                                <td><img src={Shape2} alt="location" /> {info.location}</td>
+                                <td >
+                                    <div className="first-chart" style={{ backgroundColor: backgroundColors[i], color: colors[i] }}>
+                                        {info.name.charAt(0)}
+                                    </div>
+                                </td>
+                                <td className="factory-name">{info.name}</td>
+                                <td className="location"><img src={Shape2} alt="location" /> {info.location}</td>
                                 <td>link</td>
                             </tr>
                         )
