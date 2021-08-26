a4a4447ab14cfbe0370124d9a2f8741e7adb3892
Kristina Peršić <72509184+Kristina456@users.noreply.github.com>
2021-08-26 10:02:10 +0200

    SalesReport modifications

--- src/components/sales-report/SalesReport.tsx
+++ src/components/sales-report/SalesReport.tsx
@@ -17,24 +17,25 @@ export default function SalesReport({ data }: { data: CardSalesReport[] }) {
 
     return (
         <div className="sales-report">
-            <div className="title">Sales Report</div>
-            <table>
-                <tr className="subtitles">
-                    <th>#</th>
-                    <th>PROVIDER</th>
-                    <th>STATS</th>
+            <div className="sales-report__title">Sales Report</div>
+            <table className="sales-report__table">
+                <tr className="sales-report__header">
+                    <th className="sales-report__cell">#</th>
+                    <th className="sales-report__cell">PROVIDER</th>
+                    <th className="sales-report__cell">STATS</th>
                 </tr>
                 {
                     data.map((report) => {
                         return (
-                            <tr className="informations">
-                                <td className="informations__number">{report.ordinalNumber} </td>
-                                <td className="informations__provider">{report.provider}</td>
+                            <tr className="sales-report__informations informations">
+                                <td className="informations__number ">{report.ordinalNumber} </td>
+                                <td className="informations__provider informations__row">{report.provider}</td>
                                 {report.percentage > 0 ?
-                                    <td style={{
+
+                                    <td className=" informations__row" style={{
                                         color: " rgba(91, 176, 156, 1)"
                                     }}>${report.stats}(+{report.percentage * 100}%)</td> :
-                                    <td style={{
+                                    <td className=" informations__row" style={{
                                         color: "rgba(221, 69, 69, 1)"
                                     }
                                     } >${report.stats}({report.percentage * 100} %)</td>}
